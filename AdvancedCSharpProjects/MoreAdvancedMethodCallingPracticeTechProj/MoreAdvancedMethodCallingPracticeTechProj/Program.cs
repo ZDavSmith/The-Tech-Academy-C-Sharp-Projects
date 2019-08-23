1ab59ff024dd142fed162bf04f2d80927adee839
Zachary Smith <zdavsmith@gmail.com>
2019-08-23 01:32:48 -0400

    Add more advanced classes to the program

---
+++ AdvancedCSharpProjects/MoreAdvancedMethodCallingPracticeTechProj/MoreAdvancedMethodCallingPracticeTechProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreAdvancedMethodCallingPracticeTechProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
