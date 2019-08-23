fec5ef29d9b18cfe72c8b60a7ae6eff69e4c797d
Zachary Smith <zdavsmith@gmail.com>
2019-08-23 00:12:02 -0400

    Add void method class project

---
+++ AdvancedCSharpProjects/Void_Method_Class_With_Integers/Void_Method_Class_With_Integers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void_Method_Class_With_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now type in a second number");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            //=======================
+            //Instantiating the class
+            //========================
+           mathOperations Math = new mathOperations();
+           Math.Operation(num1: userNum1,num2: userNum2);
+           
+
+        }
+    }
+}
---
+++ AdvancedCSharpTutorials/Classes_And_Objects_Tutorials/Classes_And_Objects_Tutorials/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //==============================
+            //CREATING CLASSES AND OBJECTS
+            //==============================
+
+            Deck deck = new Deck();
+            //deck = Shuffle(deck); //Shuffling the cards
+            int timesShuffled = 0;
+            deck = Shuffle(deck: deck, out timesShuffled, times: 3); //Showing Named Paramteters to make the code more readable
+    
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shuffled: {0}", timesShuffled); //String format, in brackets a 0 and then the variable ou want to be assigned to it
+            Console.ReadLine();
+        }
+
+        //===============================
+        //SHUFFLE METHOD FOR THE CARDS
+        //===============================
+
+        // int times = 1 is an optional perameter because its assigned a 1
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1) //method - public, accesible anywhere, static: dont want to create obj program before calling, Deck = type of data, Shuffle = name of function, (Deck deck) parameter of type Deck, variable name deck
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
+            {
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random(); //Random is a class from the framework we can call
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count); //min value of 0 and max value of 52
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex); // deletes it from the temporary List
+                }
+                deck.Cards = TempList;
+            }
+                return deck;
+            
+        }
+
+        //==========================================================
+        //METHOD FOR SHUFFLING AS MANY TIMES AS THE DEALER WANTS (Commented out because it was inserted into the main method)
+        //==========================================================
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
+
+
+
+    }
+}
