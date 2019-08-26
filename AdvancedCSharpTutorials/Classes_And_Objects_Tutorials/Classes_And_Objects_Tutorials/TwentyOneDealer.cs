8b5a70653d39d4f595f75b59dde34761cb9c5636
Zachary Smith <zdavsmith@gmail.com>
2019-08-26 17:39:46 -0400

    Adding more rules

---
+++ AdvancedCSharpTutorials/Classes_And_Objects_Tutorials/Classes_And_Objects_Tutorials/TwentyOneDealer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_And_Objects_Tutorials
+{
+    class TwentyOneDealer
+    {
+    }
+}
---
+++ AdvancedCSharpTutorials/Classes_And_Objects_Tutorials/Classes_And_Objects_Tutorials/TwentyOneRules.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public class TwentyOneRules
+    {
+        private static Dictionary<Face, int> _cardValues = new Dictionary<Face, int>()
+        {
+
+        };
+    }
+}
