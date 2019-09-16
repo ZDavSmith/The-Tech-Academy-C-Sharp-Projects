8540895023cd8f22b9c58b966c9f61b0be647883
Zachary Smith <zdavsmith@gmail.com>
2019-09-15 20:14:17 -0400

    Add new student information through the seed and rebuild solution

--- WebApp/UniversityMVC/UniversityMVC/DAL/SchoolInitializer.cs
+++ WebApp/UniversityMVC/UniversityMVC/DAL/SchoolInitializer.cs
@@ -16,6 +16,14 @@ namespace UniversityMVC.DAL
                 new Student{FirstName = "Zachary", LastName="Smith",EnrollmentDate=DateTime.Parse("2019-09-06")},
                 new Student{FirstName = "Jimmy", LastName="Gordan",EnrollmentDate=DateTime.Parse("2019-04-01")},
                 new Student{FirstName = "Stacy", LastName="Lewis",EnrollmentDate=DateTime.Parse("2019-07-23")},
+                new Student{FirstName = "Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                new Student{FirstName = "Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstName = "Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstName = "Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstName = "Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstName = "Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                new Student{FirstName = "Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstName = "Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
             };
 
             students.ForEach(c => context.Students.Add(c));
@@ -30,6 +38,13 @@ namespace UniversityMVC.DAL
                 new Course{CourseID = 5, Title="Adventure Education", Credits=3,},
                 new Course{CourseID = 6, Title="Physics", Credits=3,},
                 new Course{CourseID = 7, Title="Psychology", Credits=3,},
+                new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+                new Course{CourseID=1045,Title="Calculus",Credits=4,},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+                new Course{CourseID=2021,Title="Composition",Credits=3,},
+                new Course{CourseID=2042,Title="Literature",Credits=4,}
             };
             courses.ForEach(c => context.Courses.Add(c));
             context.SaveChanges();
@@ -42,6 +57,18 @@ namespace UniversityMVC.DAL
                 new Enrollment{StudentID=2,CourseID=7,Grade=Grade.F},
                 new Enrollment{StudentID=2,CourseID=4,Grade=Grade.C},
                 new Enrollment{StudentID=3,CourseID=2,Grade=Grade.D},
+                new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+                new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+                new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+                new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+                new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+                new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+                new Enrollment{StudentID=3,CourseID=1050},
+                new Enrollment{StudentID=4,CourseID=1050,},
+                new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+                new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+                new Enrollment{StudentID=6,CourseID=1045},
+                new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A}
             };
 
             enrollments.ForEach(c => context.Enrollments.Add(c));
